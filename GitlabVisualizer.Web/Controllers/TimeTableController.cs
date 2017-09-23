379b56a8245a42f845098bcf7c377ca57daf1d0c
Reci <recidivizd@gmail.com>
2017-09-23 14:33:33 +0300

    Changed default controller

---
+++ GitlabVisualizer.Web/Controllers/TimeTableController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GitlabVisualizer.Web.Controllers
+{
+    public class TimeTableController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
