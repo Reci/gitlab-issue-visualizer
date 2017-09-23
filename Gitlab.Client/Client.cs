68e887264dc5801662b3603813ed1e0d043fc0a6
Reci <recidivizd@gmail.com>
2017-09-23 21:18:11 +0300

    Added models and gitlab api client

---
+++ Gitlab.Client/Client.cs
@@ -0,0 +1,78 @@
+﻿using GitlabVisualizer.Models;
+using Newtonsoft.Json;
+using NGitLab;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitlabVisualizer.ApiClient
+{
+    public class Client
+    {
+        string endpoint = System.Configuration.ConfigurationManager.AppSettings["GitlabApiEndpoint"];
+        string privateToken = System.Configuration.ConfigurationManager.AppSettings["GitlabApiPrivateToken"];
+        GitLabClient client;
+
+        public Client()
+        {
+            client = GitLabClient.Connect(endpoint, privateToken);
+        }
+
+        public Client(string endpoint, string privateToken)
+        {
+            client = GitLabClient.Connect(endpoint, privateToken);
+        }
+
+        public IEnumerable<User> GetUsers()
+        {
+            using (var client = new WebClient())
+            {
+                client.Encoding = Encoding.UTF8;
+                var json = client.DownloadString($"{endpoint}/users?private_token={privateToken}");
+                IEnumerable<User> result = JsonConvert.DeserializeObject<List<User>>(json);
+                return result;
+            }
+        }
+
+        public IEnumerable<Project> GetProjects()
+        {
+            using (var client = new WebClient())
+            {
+                client.Encoding = Encoding.UTF8;
+                var json = client.DownloadString($"{endpoint}/projects?private_token={privateToken}");
+                IEnumerable<Project> result = JsonConvert.DeserializeObject<List<Project>>(json);
+                return result;
+            }
+        }
+
+        public IEnumerable<Issue> GetIssues()
+        {
+            var projects = GetProjects();
+            List<Issue> result = new List<Issue>();
+            using (var client = new WebClient())
+            {
+                client.Encoding = Encoding.UTF8;
+                foreach (var project in projects)
+                {
+                    var json = client.DownloadString($"{endpoint}/projects/{project.Id}/issues?private_token={privateToken}");
+                    result.AddRange(JsonConvert.DeserializeObject<List<Issue>>(json));
+                }
+            }
+            return result;
+        }
+
+        public IEnumerable<Issue> GetIssuesByProjectId(int projectId)
+        {
+            using (var client = new WebClient())
+            {
+                client.Encoding = Encoding.UTF8;
+                var json = client.DownloadString($"{endpoint}/projects/{projectId}/issues?private_token={privateToken}");
+                IEnumerable<Issue> result = JsonConvert.DeserializeObject<List<Issue>>(json);
+                return result;
+            }
+        }
+    }
+}
---
+++ GitlabVisualizer.Models/Milestone.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitlabVisualizer.Models
+{
+    
+    public class Milestone
+    {
+        public int Id;
+        public string Title;
+        public string Description;
+        public string DueDate;
+        public string State;
+        public DateTime CreatedAt;
+        public DateTime UpdatedAt;
+    }
+}
